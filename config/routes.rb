0875c40530d05ffde6043dfbd78dc274c814c09f
remy duquenne <duquenneremy@gmail.com>
2018-08-20 19:22:20 +0200

    routes validees

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'watches#research'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :watches do
+    collection do
+      get 'mine', to: "watches#mine"
+    end
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:show, :index] do
+    collection do
+      get 'my_renting', to: "bookings#my_renting"
+    end
+    member do
+      patch :accept
+      patch :decline
+    end
+  end
 end
