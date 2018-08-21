0dfb50e505113f8d74daff8550702cc09db6ce77
remy duquenne <duquenneremy@gmail.com>
2018-08-21 11:49:40 +0200

    add methods watches controller and index.html in watches view

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,22 +1,29 @@
 class WatchesController < ApplicationController
   def index
+    @watches = Watch.all
   end
 
   def show
+    @watch = Watch.find(params[:id])
   end
 
   def new
+    @watch = Watch.new
   end
 
   def create
+    @watch = Watch.new(watch_params)
+    if @watch.save!
+      redirect_to @watch, notice: 'Watch was successfully created.'
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+  private
 
-  def update
-  end
+  def watch_params
+      params.require(:watch).permit(:price, :location, :gender, :color, :material, :name, :image, :description, :brand)
+    end
 
-  def delete
-  end
 end
