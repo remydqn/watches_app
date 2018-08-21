3d4d1bc012aaa566ff239d9ca9d35d979ee444de
remy duquenne <duquenneremy@gmail.com>
2018-08-21 11:29:32 +0200

    roots changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'watches#research'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :watches do
-    collection do
-      get 'mine', to: "watches#mine"
-    end
-    resources :bookings, only: [:new, :create]
+  resources :watches, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:create]
   end
 
-  resources :bookings, only: [:show, :index] do
-    collection do
-      get 'my_renting', to: "bookings#my_renting"
-    end
+  resources :bookings, only: [:index, :show] do
     member do
-      patch :accept
-      patch :decline
+      patch 'accept'
+      patch 'decline'
     end
   end
 end
